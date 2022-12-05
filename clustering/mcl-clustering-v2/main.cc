fd0f3c87557916c9ef1990af3bb66a3b8e114a34
Charlie Sale <softwaresale01@gmail.com>
2022-12-05 02:14:46 -0500

    Parallelize building scorers

--- clustering/mcl-clustering-v2/main.cc
+++ clustering/mcl-clustering-v2/main.cc
@@ -24,13 +24,14 @@ int main(int argc, char **argv) {
     // The first arg is the path to the database file to cluster
     auto regexes = rereuse::db::read_patterns_from_path(argv[1]);
 
-    // Make scorers for everything
-    unsigned long id = 0;
     // NOTE the shared_ptr here is very deliberate. This is to ensure that the string's file associated with the
     // object doesn't get cleaned up. When moving, the original delete constructor will get called, and so the
     // strings file gets cleaned up. The better thing to do is to manually add in ref counting to the object and only
     // delete the strings file once the ref count hits zero
     std::vector<std::shared_ptr<RexSimilarityScorer>> scorers;
+#if 0
+    unsigned long id = 0;
+
     for (const auto &regex : regexes) {
         try {
             auto scorer = std::make_shared<RexSimilarityScorer>(regex, id, wrapper);
@@ -41,6 +42,29 @@ int main(int argc, char **argv) {
             spdlog::warn("Skipping regex /{}/: {}", regex, err.what());
         }
     }
+#else
+    std::atomic<std::size_t> id;
+    std::vector<std::future<std::shared_ptr<RexSimilarityScorer>>> tasks;
+    ThreadPool tp(8);
+    for (const auto &regex : regexes) {
+        auto task = tp.enqueue([regex, &id, &wrapper] {
+            try {
+                auto scorer = std::make_shared<RexSimilarityScorer>(regex, id++, wrapper);
+                return scorer;
+            } catch (std::runtime_error &exe) {
+                spdlog::warn("Skipping regex /{}/: {}", regex, exe.what());
+                return std::shared_ptr<RexSimilarityScorer>();
+            }
+        });
+        tasks.push_back(std::move(task));
+    }
+
+    for (auto &task : tasks) {
+        auto scorer = task.get();
+        if (scorer)
+            scorers.push_back(std::move(scorer));
+    }
+#endif
 
     SimilarityTable<RexSimilarityScorer> similarity_table(std::move(scorers));
     similarity_table.compute();
