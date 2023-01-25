54442a55fa2259ee7e869af0988cc1b54acfb9c2
Charlie Sale <softwaresale01@gmail.com>
2023-01-25 17:53:53 -0500

    add parallelism to string gen tool

--- clustering/rex-string-generator/main.cc
+++ clustering/rex-string-generator/main.cc
@@ -3,10 +3,14 @@
 //
 
 #include <iostream>
+#include <thread>
+
 #include "nlohmann/json.hpp"
 
 #include "rex_wrapper.h"
 #include "librereuse/db/pattern_reader.h"
+#include "librereuse/util/chunk.h"
+#include "ThreadPool.h"
 
 #define WINE_PATH "/usr/bin/wine"
 #define REX_PATH "/home/cmsale/Rex.exe"
@@ -24,25 +28,41 @@ int main(int argc, char **argv) {
     // Read in all the regexes
     auto input_regexes = rereuse::db::read_patterns_from_path(argv[1]);
 
+    // chunk everything
+    auto input_chunks = rereuse::util::chunk(std::move(input_regexes), std::thread::hardware_concurrency());
+
     // Setup up rex
     RexWrapper wrapper(rex_path.data(), wine_path.data());
+    ThreadPool thread_pool(std::thread::hardware_concurrency());
 
-    // For each regex, generate 100 rex strings for each
-    std::unordered_map<std::string, std::vector<std::string>> rex_strings;
-    for (auto &input_regex : input_regexes) {
-        try {
-            auto generated_strings = wrapper.generate_strings(input_regex, 100);
-            // try moving all the resources into the map to reduce memory issues??
-            auto pair = std::make_pair(std::move(input_regex), std::move(generated_strings));
-            rex_strings.insert(std::move(pair));
-        } catch (std::runtime_error &exe) {
-            // skip the ones that don't work
-            continue;
-        }
+    std::vector<std::future<std::unordered_map<std::string, std::vector<std::string>>>> tasks;
+    for (auto &chunk : input_chunks) {
+        auto task = thread_pool.enqueue([&wrapper, regexes = std::move(chunk)] {
+            std::unordered_map<std::string, std::vector<std::string>> rex_strings;
+            for (const auto &regex : regexes) {
+                try {
+                    auto strings = wrapper.generate_strings(regex, 100);
+                    auto pair = std::make_pair(regex, std::move(strings));
+                    rex_strings.insert(std::move(pair));
+                } catch (std::runtime_error &exe) {
+                    continue;
+                }
+            }
+
+            return rex_strings;
+        });
+        tasks.push_back(std::move(task));
+    }
+
+    // merge all the tasks into one map
+    std::unordered_map<std::string, std::vector<std::string>> all_strings;
+    for (auto &task : tasks) {
+        auto strings = task.get();
+        all_strings.merge(std::move(strings));
     }
 
     // Serialize and output
-    nlohmann::json output_obj = rex_strings;
+    nlohmann::json output_obj = all_strings;
 
     std::ofstream output(argv[2]);
     output << output_obj << std::endl;
